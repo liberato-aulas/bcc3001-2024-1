2f03e1c9da90d388ec457a78db41830bb7051f82
liberato-utfpr <liberato@utfpr.edu.br>
2024-04-26 16:02:39 -0300

    aula 24-04-2024

---
+++ 02 - Lista/contigua/listaContigua.cpp
@@ -0,0 +1,53 @@
+
+#include <string>
+
+class ListaContigua {
+
+  private:
+   int* array;
+   int capacity;
+   int size;
+
+  public:
+   ListaContigua();
+   ~ListaContigua();
+
+   // Insere o elemento e na última posição
+   bool push_back(int e);
+   // Insere o elemento e na primeira posição
+   bool push_front(int e);
+   // Insere o elemento e na posição pos
+   bool insert(int pos, int e);
+
+   // Remove o último elemento
+   int pop_back();
+   // Remove o primeiro elemento
+   int pop_front();
+   // Remove o elemento da posição pos e retorna o elemento removido
+   int erase(int pos);
+
+   // Retorna o primeiro elemento
+   int front();
+   // Retorna o último elemento
+   int back();
+   // Retorna o elemento da posição pos
+   int at(int pos);
+
+   // Torna a lista vazia
+   void clear();
+
+   // Verifica se o vetor está vazio
+   bool empty();
+   // Devolve a quantidade de elementos
+   int size();
+   // Substitui o elemento da posição pos pelo elemento e
+   bool replace(int pos, int e);
+
+   // Imprime todos os elementos no formato [1,2,3]
+   void print();
+
+  private:
+   // Dobrar a capacidade quando não houver mais espaço
+   // Reduzir a capacidade pela metade quando for inferior a 25% de ocupação
+   void gerenciaCapacidade();
+};
---
+++ 02 - Lista/organizacaoEncadeada/encadeamento.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+// typedef struct no {
+//     int dado;
+//     struct no* prox;
+// } No;
+
+class No {
+  public:
+   int dado;
+   No* prox;
+
+   No() {}
+   No(int dado) {}
+   No(int dado, No* prox) {}
+};
+
+void printEncadeada(No* no) {
+
+   No* tmp = no;
+
+   cout << "[";
+   while (tmp != nullptr) {
+      cout << tmp->dado;
+      tmp = tmp->prox;
+      if (tmp != nullptr) cout << ",";
+   }
+   cout << "]" << endl;
+}
+
+void printContigua(int* v, int tam) {
+   cout << "[";
+   for (int i = 0; i < tam; i++) {
+      cout << v[i];
+      if (i < tam - 1) cout << ",";
+   }
+   cout << "]" << endl;
+}
+
+int main() {
+
+   int* organizacaoContigua = new int[10];
+   organizacaoContigua[0] = 1;
+   organizacaoContigua[1] = 2;
+   organizacaoContigua[2] = 3;
+
+   //    No* n1 = (No*) malloc(sizeof(No));
+   //    No* n2 = (No*) malloc(sizeof(No));
+   //    No* n3 = (No*) malloc(sizeof(No));
+
+   No* n1 = new No(1);
+   No* n2 = new No();
+   No* n3 = new No(3, nullptr);
+
+   //    n1->dado = 1;
+   n2->dado = 2;
+   n3->dado = 3;
+
+   n1->prox = n2;
+   n2->prox = n3;
+   n3->prox = nullptr;
+
+   n3->prox = new No();
+   n3->prox->dado = 4;
+   n3->prox->prox = nullptr;
+
+   No* organizacaoEncadeada = n1;
+   n1 = nullptr;
+   n2 = nullptr;
+   n3 = nullptr;
+
+   printContigua(organizacaoContigua, 3);
+   printEncadeada(organizacaoEncadeada);
+}
